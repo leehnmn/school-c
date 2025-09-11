0c22e3fba56ff7fefbdc5e518f09e063e4717601
leehnmn <dlgusals0120@gmail.com>
2025-09-11 15:32:43 +0900

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	printf("This is my first program!\n");
+	printf("Hi, Human!\n");
+	printf("Hi, Computer!\n");
+	printf("Hello, world!\n");    /*주석열심히 다세요*/
+	return 0;
+
+}
+
+/*
+#include <stdio.h>
+
+int main() {
+
+	printf("Hello, world!\n");
+	return 0;
+}
+*/
