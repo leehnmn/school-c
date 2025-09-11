b8996c6f3947b150b54977776455b5c73b65c089
I-14 <I-14@I-14>
2025-09-11 15:36:07 +0900

    c언어 9/11 일자 수업내용 입니다.

--- Project1/Project1/pro9/Hello.c
+++ Project1/Project1/pro9/Hello.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main() {
-	
+	printf("This is my first program!\n");
+	printf("Hi, Human!\n");
+	printf("Hi, Computer!\n");
 	printf("Hello, world!\n");    /*주석열심히 다세요*/
 	return 0;
 
