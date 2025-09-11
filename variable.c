30f76d63de2800da38c8ac3a9354a5718cd5c633
I-14 <I-14@I-14>
2025-09-11 16:04:21 +0900

    9/11일자 변수 프로그램입니다.

--- variable.c
+++ variable.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
-int main() {
-	
+int main() 
+{
 	int a;
-	a = 10;
-	
-	printf("a=%d \n", a); //변수 a를 정수로 출력함
+	a = 127;
+
+	printf("a=%3o in %d-number system \n", a, 8);
+	printf("a=%3d in %d-number system \n", a, 10);
+	printf("a=%3x in %d-number system \n", a, 16);
 	return 0;
-}	
+}
+
+
+
+
+//#include <stdio.h>
+//
+//int main() {
+//
+//	int a;
+//	a = 10;
+//
+//	printf("a=%d \n", a); //변수 a를 정수로 출력함
+//	return 0;
+//}
